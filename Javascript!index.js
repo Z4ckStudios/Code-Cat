113a57e46359b2da9cf9348b513972c3d94c0bfc
Z4ck <128758112+Z4ckStudios@users.noreply.github.com>
2024-08-10 18:03:34 -0600

    Add files via upload

--- Javascript!index.js
+++ Javascript!index.js
@@ -23,25 +23,225 @@ var DiscordLogo = document.querySelector(".DiscordLogo");
 var LWStatus = localStorage.getItem("LWStatus");
 var ZSCStatus = localStorage.getItem("ZSCStatus");
 
-if(LWStatus == "Open")
+var pagesWrapper = document.querySelector(".pagesWrapper");
+var Page1 = document.querySelector(".Page1");
+var Page2 = document.querySelector(".Page2");
+var Page3 = document.querySelector(".Page3");
+var Page4 = document.querySelector(".Page4");
+var Page5 = document.querySelector(".Page5");
+
+var dockSwitcher = document.querySelector(".dockSwitcher");
+var pageSwitcher = document.querySelector(".pageSwitcher");
+var CurrentSwitcher = localStorage.getItem("CurrentSwitcher");
+var Pages = localStorage.getItem("Pages");
+
+if(CurrentSwitcher == null, "pageSwitcher")
+{pageSwitcher.style.display = "inline";
+dockSwitcher.style.display = "none";
+pagesWrapper.style.display = "inline";}
+
+if(CurrentSwitcher == "dockSwitcher")
+{pageSwitcher.style.display = "none";
+dockSwitcher.style.display = "inline";
+pagesWrapper.style.display = "none";}
+
+if(Pages == null, "true")
+{LinksWrapper.style.left = "-20%";
+OSMPanelB.style.display = "none";
+CSMPanelB.style.display = "none";
+
+ZSChangelog.style.left = "115%";
+OChangelogB.style.display = "none";
+CChangelogB.style.display = "none";}
+
+if(LWStatus == "Open" && Pages == "false")
 {LinksWrapper.style.left = "0.5%";
 CSMPanelB.style.display = "inline";
 OSMPanelB.style.display = "none";}
 else
-if(LWStatus == null, "Closed")
+if(LWStatus == null, "Closed" && Pages == "false")
 {LinksWrapper.style.left = "-20%";
 OSMPanelB.style.display = "inline";
 CSMPanelB.style.display = "none";}
 
-if(ZSCStatus == null, "Open")
+if(ZSCStatus == "Closed" && Pages == "false")
+{ZSChangelog.style.left = "115%";
+OChangelogB.style.display = "inline";
+CChangelogB.style.display = "none";}
+else
+if(ZSCStatus == null, "Open" && Pages == "false")
 {ZSChangelog.style.left = "83.5%";
 CChangelogB.style.display = "inline";
 OChangelogB.style.display = "none";}
 
-if(ZSCStatus == "Closed")
-{ZSChangelog.style.left = "115%";
-OChangelogB.style.display = "inline";
-CChangelogB.style.display = "none";}
+document.querySelector(".pageSwitcher").addEventListener("click",
+function() {
+
+    localStorage.setItem("Pages", "false");
+
+    pageSwitcher.style.display = "none";
+    dockSwitcher.style.display = "inline";
+
+    localStorage.setItem("CurrentSwitcher", "dockSwitcher");
+
+    pagesWrapper.style.display = "none";
+
+    if(LWStatus == "Open")
+        {LinksWrapper.style.left = "0.5%";
+        CSMPanelB.style.display = "inline";
+        OSMPanelB.style.display = "none";}
+        else
+        if(LWStatus == null, "Closed")
+        {LinksWrapper.style.left = "-20%";
+        OSMPanelB.style.display = "inline";
+        CSMPanelB.style.display = "none";}
+        
+        if(ZSCStatus == "Closed")
+        {ZSChangelog.style.left = "115%";
+        OChangelogB.style.display = "inline";
+        CChangelogB.style.display = "none";}
+        else
+        if(ZSCStatus == null, "Open")
+        {ZSChangelog.style.left = "83.5%";
+        CChangelogB.style.display = "inline";
+        OChangelogB.style.display = "none";}
+})
+document.querySelector(".pageSwitcher").addEventListener("mouseover",
+function() {
+
+    pageSwitcher.style.opacity = "1";
+    pageSwitcher.style.border = "1px solid rgb(0, 255, 255)";
+    pageSwitcher.style.background = "none";
+    pageSwitcher.style.cursor = "pointer";
+})
+document.querySelector(".pageSwitcher").addEventListener("mouseout",
+function() {
+
+    pageSwitcher.style.opacity = "0.5";
+    pageSwitcher.style.border = "none";
+    pageSwitcher.style.background = "rgb(15, 15, 15)";
+    pageSwitcher.style.cursor = "default";
+})
+
+document.querySelector(".dockSwitcher").addEventListener("click",
+function() {
+
+    localStorage.setItem("Pages", "true");
+
+    pageSwitcher.style.display = "inline";
+    dockSwitcher.style.display = "none";
+
+    localStorage.setItem("CurrentSwitcher", "pageSwitcher");
+
+    pagesWrapper.style.display = "inline";
+
+    LinksWrapper.style.left = "-20%";
+    OSMPanelB.style.display = "none";
+    CSMPanelB.style.display = "none";
+
+    ZSChangelog.style.left = "115%";
+    OChangelogB.style.display = "none";
+    CChangelogB.style.display = "none";
+})
+document.querySelector(".dockSwitcher").addEventListener("mouseover",
+function() {
+
+    dockSwitcher.style.opacity = "1";
+    dockSwitcher.style.cursor = "pointer";
+    dockSwitcher.style.textDecoration = "underline";
+})
+document.querySelector(".dockSwitcher").addEventListener("mouseout",
+function() {
+
+    dockSwitcher.style.opacity = "0.5";
+    dockSwitcher.style.cursor = "default";
+    dockSwitcher.style.textDecoration = "none";
+})
+
+document.querySelector(".Page1").addEventListener("click",
+function() {
+
+    
+})
+document.querySelector(".Page1").addEventListener("mouseover",
+function() {
+
+    Page1.style.opacity = "1";
+    Page1.style.textDecoration = "underline";
+    Page1.style.cursor = "pointer";
+})
+document.querySelector(".Page1").addEventListener("mouseout",
+function() {
+
+    Page1.style.opacity = "0.5";
+    Page1.style.textDecoration = "none";
+    Page1.style.cursor = "default";
+})
+
+document.querySelector(".Page2").addEventListener("click",
+function() {
+
+    location.replace("Page101.html");
+})
+document.querySelector(".Page2").addEventListener("mouseover",
+function() {
+
+    Page2.style.opacity = "1";
+    Page2.style.textDecoration = "underline";
+    Page2.style.cursor = "pointer";
+})
+document.querySelector(".Page2").addEventListener("mouseout",
+function() {
+
+    Page2.style.opacity = "0.5";
+    Page2.style.textDecoration = "none";
+    Page2.style.cursor = "default";
+})
+
+document.querySelector(".Page3").addEventListener("mouseover",
+function() {
+
+    Page3.style.opacity = "1";
+    Page3.style.textDecoration = "underline";
+    Page3.style.cursor = "pointer";
+})
+document.querySelector(".Page3").addEventListener("mouseout",
+function() {
+
+    Page3.style.opacity = "0.5";
+    Page3.style.textDecoration = "none";
+    Page3.style.cursor = "default";
+})
+
+document.querySelector(".Page4").addEventListener("mouseover",
+function() {
+
+    Page4.style.opacity = "1";
+    Page4.style.textDecoration = "underline";
+    Page4.style.cursor = "pointer";
+})
+document.querySelector(".Page4").addEventListener("mouseout",
+function() {
+
+    Page4.style.opacity = "0.5";
+    Page4.style.textDecoration = "none";
+    Page4.style.cursor = "default";
+})
+
+document.querySelector(".Page5").addEventListener("mouseover",
+function() {
+
+    Page5.style.opacity = "1";
+    Page5.style.textDecoration = "underline";
+    Page5.style.cursor = "pointer";
+})
+document.querySelector(".Page5").addEventListener("mouseout",
+function() {
+
+    Page5.style.opacity = "0.5";
+    Page5.style.textDecoration = "none";
+    Page5.style.cursor = "default";
+})
 
 document.querySelector(".OSMPanelB").addEventListener("click",
 function() {
@@ -146,7 +346,7 @@ function() {
 document.querySelector(".TwitterBCover").addEventListener("click",
 function() {
 
-    location.replace("https://twitter.com/Z4ckStudios");
+    window.open("https://twitter.com/Ph4nt0mZStudios");
 })
 document.querySelector(".TwitterBCover").addEventListener("mouseover",
 function() {
@@ -165,7 +365,7 @@ function() {
 document.querySelector(".YTBCover").addEventListener("click",
 function() {
 
-    location.replace("https://youtube.com/@Z4ckStudios");
+    window.open("https://youtube.com/@Z4ckStudios");
 })
 document.querySelector(".YTBCover").addEventListener("mouseover",
 function() {
@@ -184,7 +384,7 @@ function() {
 document.querySelector(".TwitchBCover").addEventListener("click",
 function() {
 
-    location.replace("https://twitch.tv/Z4ckStudios");
+    window.open("https://twitch.tv/Z4ckStudios");
 })
 document.querySelector(".TwitchBCover").addEventListener("mouseover",
 function() {
@@ -203,7 +403,22 @@ function() {
 document.querySelector(".DiscordBCover").addEventListener("click",
 function() {
 
-    location.replace("https://discord.gg/f5PdW5TDCj");
+    //window.open("https://discord.gg/");
+    DiscordB.innerHTML = "Inactive";
+    DiscordB.style.color = "red";
+
+    let DSBTimer = 0;
+    const DSBInterval = setInterval(DSBInt, 1000);
+
+    function DSBInt() {
+        DSBTimer++
+
+        if(DSBTimer < 3)
+        {DiscordB.innerHTML = "Discord";
+        DiscordB.style.color = "white";}
+        
+        if(DSBTimer < 3) {clearInterval(DSBInterval);}
+    }
 })
 document.querySelector(".DiscordBCover").addEventListener("mouseover",
 function() {
