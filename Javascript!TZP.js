507b78cf0605307a61c25bec0a56b2045ae26b21
Z4ck <128758112+Z4ckStudios@users.noreply.github.com>
2024-11-01 10:18:13 -0600

    Z4ck Studios - Beta v1.14
    - Added TZP Menu

--- Javascript!TZP.js
+++ Javascript!TZP.js
@@ -5,11 +5,18 @@ window.history.replaceState(null, document.title, url);
 var body = document.querySelector("body");
 var eventTimer = document.querySelector(".eventTimer");
 var homeButton = document.querySelector(".homeButton");
+var TZPDarken = document.querySelector(".TZPDarken");
+var FLBackground = document.querySelector(".FLBackground");
+var TCTSText = document.querySelector(".TCTSText");
+var FLTitle = document.querySelector(".FLTitle");
+var TZPMix = document.querySelector(".TZPMix");
+var TheZenProject = document.querySelector(".TheZenProject");
+var boxSep = document.querySelector(".boxSep");
 
 const countdownInterval = setInterval(countdownTimer, 1000);
 
 function countdownTimer(){
-    const time = new Date(`October 31, 2024 23:45:00`);
+    const time = new Date(`October 31, 2024 22:00:00`);
     const currentTime = new Date();
 
     const difference = time - currentTime;
@@ -59,10 +66,12 @@ function countdownTimer(){
     if(days == 0 && hours == 0 && minutes == 0 && seconds <= 10 && eventTimer.innerHTML == `${seconds}s`)
     {eventTimer.style.color = "#FF0000";}
 
-    if(currentTime > new Date(`October 31, 2024 23:45:00`))
+    if(currentTime > new Date(`October 31, 2024 22:00:00`))
     {eventTimer.innerHTML = "0"
     eventTimer.style.display = 'none';
-    body.style.background = "#FF0000"
+    body.style.background = "black";
+    TZPDarken.style.display = "none";
+    TheZenProject.style.display = "inline";
     clearInterval(countdownInterval);}
 }
 
@@ -86,4 +95,62 @@ function() {
     homeButton.style.cursor = "default";
     homeButton.style.background = "transparent";
     homeButton.style.color = "white";
+})
+
+TCTSText.addEventListener("mouseover",
+function() {
+
+    TCTSText.style.cursor = "pointer";
+    TCTSText.style.borderBottom = "2px solid #0000FF";
+})
+TCTSText.addEventListener("mouseout",
+function() {
+
+    TCTSText.style.cursor = "default";
+    TCTSText.style.borderBottom = "none";
+})
+
+TZPMix.addEventListener("click",
+function() {
+
+    FLTitle.style.display = "none";
+    TCTSText.style.display = "none";
+    eventTimer.style.display = 'inline';
+    FLBackground.style.display = "none";
+    boxSep.style.display = "none";
+    TZPMix.style.display = "none";
+
+    eventTimer.innerHTML = "See you soon.. :)";
+
+    let SYSTimer = 0;
+    const SYSInterval = setInterval(SYSInt, 1000);
+
+    function SYSInt() {
+        SYSTimer++
+
+        if(SYSTimer > 1)
+        {FLTitle.style.display = "inline";
+        TCTSText.style.display = "inline";
+        eventTimer.style.display = 'none';
+        FLBackground.style.display = "inline";
+        boxSep.style.display = "inline";
+        TZPMix.style.display = "inline";
+
+        eventTimer.innerHTML = "0";
+        clearInterval(SYSInterval);}
+    }
+})
+TZPMix.addEventListener("mouseover",
+function() {
+
+    TZPMix.style.cursor = "pointer";
+    TZPMix.style.borderBottom = "2px solid #0000FF";
+    TZPMix.style.opacity = "0.2";
+})
+TZPMix.addEventListener("mouseout",
+function() {
+
+    TZPMix.style.cursor = "default";
+    TZPMix.style.borderBottom = "none";
+    TZPMix.style.opacity = "0.1";
 })
